0c6842d4b57b118426355c455157d0638d5889b7
sophiabersani <soph.bersani@gmail.com>
2022-11-11 11:04:43 -0600

    Update

---
+++ Developer Console.cmd
@@ -0,0 +1,34 @@
+@echo off
+
+echo "Opening Stable Diffusion UI - Developer Console.." & echo.
+
+set PATH=C:\Windows\System32;%PATH%
+
+@rem set legacy and new installer's PATH, if they exist
+if exist "installer" set PATH=%cd%\installer;%cd%\installer\Library\bin;%cd%\installer\Scripts;%cd%\installer\Library\usr\bin;%PATH%
+if exist "installer_files\env" set PATH=%cd%\installer_files\env;%cd%\installer_files\env\Library\bin;%cd%\installer_files\env\Scripts;%cd%\installer_files\Library\usr\bin;%PATH%
+
+set PYTHONPATH=%cd%\installer;%cd%\installer_files\env
+
+@rem activate the installer env
+call conda activate
+
+@rem Test the environment
+echo "Environment Info:"
+call where git
+call git --version
+
+call where conda
+call conda --version
+
+echo.
+
+@rem activate the environment
+call conda activate .\stable-diffusion\env
+
+call where python
+call python --version
+
+echo.
+
+cmd /k
---
+++ Start Stable Diffusion UI.cmd
@@ -0,0 +1,27 @@
+@echo off
+
+cd /d %~dp0
+set PATH=C:\Windows\System32;%PATH%
+
+@rem set legacy installer's PATH, if it exists
+if exist "installer" set PATH=%cd%\installer;%cd%\installer\Library\bin;%cd%\installer\Scripts;%cd%\installer\Library\usr\bin;%PATH%
+
+@rem Setup the packages required for the installer
+call scripts\bootstrap.bat
+
+@rem set new installer's PATH, if it downloaded any packages
+if exist "installer_files\env" set PATH=%cd%\installer_files\env;%cd%\installer_files\env\Library\bin;%cd%\installer_files\env\Scripts;%cd%\installer_files\Library\usr\bin;%PATH%
+
+set PYTHONPATH=%cd%\installer;%cd%\installer_files\env
+
+@rem Test the bootstrap
+call where git
+call git --version
+
+call where conda
+call conda --version
+
+@rem Download the rest of the installer and UI
+call scripts\on_env_start.bat
+
+@pause
